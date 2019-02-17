a891ca0cc37fc372eaa1ee79ce97f2a83f29078a
Ghada <ghada@Ghada.local>
2019-02-17 22:11:51 +0300

    Ghada Bin Farhan clock

--- dom/js/main.js
+++ dom/js/main.js
@@ -1,82 +1,24 @@
-// console.log(document.getElementById("myid"))
-// console.log(document.getElementsByClassName("hot")[0])
-// console.log(document.querySelectorAll(".hot"))
-// console.log(document.querySelector("#myid"))
-
-//selects all classes with class name hot
-var hot = document.getElementsByClassName("hot")
-
-hot[2].style.color = "red" //changes text color
-hot[0].setAttribute("data-id", "blue") //set attribute to element
-//<li class="hot" data-id="blue">two</li>
-hot[0].className = "blue"//<li class="blue">two</li>
-
-var list = document.getElementById("list") //gets single element
-var item = document.createElement("li") //creates element
-//<li></li>
-item.setAttribute("class", "hot")
-//<li class="hot"></li>
-// var text = document.createTextNode("five")
-// five
-item.appendChild(document.createTextNode("five"))
-//<li class="hot">five</li> 
-list.appendChild(item)
-//
-function add(){
-    var input = document.querySelector("#info").value
-    
-    if(input === ""){
-    // console.log(input)
-    let items = document.createElement("li")
-    items.appendChild(document.createTextNode(input))
-
-    list.appendChild(items)
-
-    document.querySelector("#info").value = ""
-    }
-}
+var hourHand = document.getElementById('hourHand');
+var minHand = document.getElementById('minHand');
+var secHand = document.getElementById('secHand');
 
 
 
-document.querySelector("#btn").addEventListener("click",createTable)
-// document.querySelector("#btn").onclick = createTable
-//Read about parentNodes, SiblingNodes, childnodes
-//.parentNode 
-//e.target
+function clock() {
+    var time = new Date();
 
-//create table
-function createTable(e){
-    e.preventDefault()
-    e.target
-   //select table container
-    var tbl = document.querySelector(".tbl")
+    var hour = time.getHours();
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
 
-    var row_val = document.querySelector("#row").value
-    var col_val = document.querySelector("#col").value
+    var hourDeg = (hour * 30) + (0.5 * min);
+    var minDeg = (min * 6) + (0.1 * sec);
+    var secDeg = sec * 6;
 
-    //create table element
-    var table = document.createElement("table")
-    var tbody = document.createElement("tbody")
-    
-    
+    hourHand.style.transform = 'rotate(' + hourDeg + 'deg)';
+    minHand.style.transform = 'rotate(' + minDeg + 'deg)';
+    secHand.style.transform = 'rotate(' + secDeg + 'deg)';
 
-    for(var i = 0; i < row_val; i++){
-
-        var tr = document.createElement("tr")
-        for(var j = 0; j < col_val; j++){
-
-            var td = document.createElement("td")
-            td.setAttribute("onclick", "style.color ='red'")
-            var text = document.createTextNode(i + ", " + j)
-                                    //`${i}, ${j}`
-            td.appendChild(text)
-
-            tr.appendChild(td)
-            
-        }
-        tbody.appendChild(tr)
-    }
-    table.appendChild(tbody)
-    tbl.appendChild(table)
-
-}
+    setTimeout(clock, 1000);
+};
+clock();
